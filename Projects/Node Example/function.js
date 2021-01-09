09d4c5f51d7fc75ad223c38af4f7845fb0fe8d56
Muhammad Waqas <waqas.attary@gmail.com>
2021-01-09 22:23:53 +0500

    commit

--- Projects/Node Example/function.js
+++ Projects/Node Example/function.js
@@ -1,12 +1,9 @@
 function myFunc () {
     console.log("================================================");
 }
-
 var foo = 5
 module.exports =  {
     myFunc:     myFunc,
     foo:        foo
 }
-
-
 console.log(module);
