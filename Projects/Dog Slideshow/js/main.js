daa90d0b7cf0be7f17d64b7b1a229b655e6f877c
Muhammad Waqas <waqas.attary@gmail.com>
2021-01-13 00:13:51 +0500

    nextSlide

--- Projects/Dog Slideshow/js/main.js
+++ Projects/Dog Slideshow/js/main.js
@@ -3,8 +3,8 @@ let deleteFirstPhotoDelay
 
 async function start() {
   try {
-    const response = await fetch("https://dog.ceo/api/breeds/list/all")
-    const data = await response.json()
+    const response  = await fetch("https://dog.ceo/api/breeds/list/all")
+    const data      = await response.json()
     createBreedList(data.message)
   } catch (e) {
     console.log("There was a problem fetching the breed list.")
