b08a2a9530f80949a296fbf1d32d246757951c16
Muhammad Waqas <waqas.attary@gmail.com>
2021-01-13 00:14:49 +0500

    nextSlide

--- Projects/Node Example/function.js
+++ Projects/Node Example/function.js
@@ -1,9 +1,11 @@
 function myFunc () {
     console.log("================================================");
 }
+
 var foo = 5
 module.exports =  {
     myFunc:     myFunc,
     foo:        foo
 }
+
 console.log(module);
